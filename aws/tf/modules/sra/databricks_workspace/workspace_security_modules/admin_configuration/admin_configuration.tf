6bcd1c6d2327434e2ce88dd318d5b17ad5d22281
Laurent Chindeko <laurent.chindeko.hk@gmail.com>
2024-03-05 19:47:48 +0800

    Fix Workspace config issue

--- aws/tf/modules/sra/databricks_workspace/workspace_security_modules/admin_configuration/admin_configuration.tf
+++ aws/tf/modules/sra/databricks_workspace/workspace_security_modules/admin_configuration/admin_configuration.tf
@@ -2,13 +2,13 @@
 
 resource "databricks_workspace_conf" "just_config_map" {
   custom_config = {
-    "enableResultsDownloading"      = "false",
-    "enableNotebookTableClipboard"  = "false",
+    //"enableResultsDownloading"      = "false",
+    "enableNotebookTableClipboard"  = "true",
     "enableVerboseAuditLogs"        = "true",
     "enable-X-Frame-Options"        = "true",
     "enable-X-Content-Type-Options" = "true",
     "enable-X-XSS-Protection"       = "true",
-    "enableWebTerminal"             = "false",
+    "enableWebTerminal"             = "true",
     "enableDbfsFileBrowser"         = "false",
     "enforceUserIsolation"          = "true",
     "enableNotebookGitVersioning"   = "true"
